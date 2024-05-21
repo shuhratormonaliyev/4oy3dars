29bdbcf6b79d58a06136d0d5d8f4507b043226e9
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-05-21 23:22:54 +0500

    threetest

---
+++ js/index.js
@@ -0,0 +1,242 @@
+// 1 - misol;
+// let A = +prompt("sonni kiriting");
+// if (A > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 2 - misol;
+
+// let A = +prompt("sonni kiriting");
+
+// if (A % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 3 - MISOL
+
+// let A = +prompt("sonni kiriting");
+
+// if (A % 2 == 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//  4 -MISOL
+
+// let A = +prompt('a ni kiriting');
+// let B = +prompt('b ni  kiriting');
+
+// if (A > 2 && B <= 3) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//  5 - misol
+
+// let A = +prompt('a ni kiriting');
+// let B = +prompt('b ni  kiriting');
+
+// if (A >= 0 || B < -2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//  6 - misol
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni  kiriting');
+// let c = +prompt('c ni  kiriting');
+
+// if (a <= b && b <= a) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 7 - misol
+
+// let = prompt('Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "B soni A va C sonlari orasida yotadi');
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni  kiriting');
+// let c = +prompt('c ni  kiriting');
+
+// if (a <= b && b <= c || c <= b && b <= a) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 8 - misol
+
+// let = prompt("Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: A va B sonlari toq sonlar");
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+
+// if (a % 2 == 1 && b % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 9 -misol
+
+// let = prompt('Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring A va B sonlarning hech bolmaganda bittasi toq son');
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+
+// if (a % 2 == 0 && b % 2 == 1 || a % 2 == 1 && b % 2 == 0 ) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 10 - misol
+
+// let = prompt(' Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A va B sonlarning faqat bittasi toq son');
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+
+// if (a % 2 == 1 || b % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//  11- misol
+
+// let = prompt('Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A va B sonlarining har ikkalasi harn yoki toq son yoki juft son');
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+
+// if (a % 2 == 1 && b % 2 == 1 || a % 2 == 0 && b % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 12 - misol
+
+// let = prompt('Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlarning har biri musbat');
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+// let c = +prompt("c ni  kiriting");
+
+// if (a > 0 && b > 0 && c > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 13 - misol
+
+// let = prompt('Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlarning hech bolmaganda bittasi musbat');
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+// let c = +prompt("c ni  kiriting");
+
+// if  (a > 0 || b > 0 || c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// 14 - misol 
+
+// let = prompt('Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring "A, B, C sonlaridan faqat bittasi musbat son');
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+// let c = +prompt("c ni  kiriting");
+
+// if (a > 0 && b <= 0 && c <= 0 || a <= 0 && b > 0 && c <= 0 || a <= 0 && b <= 0 && c > 0 ) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+
+// 15 - misol 
+
+// let = prompt('Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlardan faqat ikkitasi musbat son".');
+
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+// let c = +prompt("c ni  kiriting");
+
+// if (a > 0 && b > 0 && c < 0 || a < 0 && b > 0 && c > 0 || a > 0 && b < 0 && c > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//  16 -misol 
+
+// let = prompt('Musbat butun son berilgan. Jumlani rostlikka tekshiring "Berilgan son ikki xonali son juft son');
+
+// let n = +prompt('sonni kiriting');
+
+// if (n > 9 && n < 100 && n % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//  17 - misol 
+
+// let n = +prompt('sonni kiriting');
+
+// if (n > 99 && n < 1000 && n % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 18 - misol 
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+// let c = +prompt("c ni  kiriting");
+
+// if (a === b && a == b && b === c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//  19 - misol 
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni  kiriting");
+// let c = +prompt("c ni  kiriting");
+
+// if (a === -b && a === -c && b === -c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// //  20 - misol 
+
+// let n = +prompt('sonni kiriting');
+
+// if (n >= 100 && n <= 999) {
+
+// }
+
+
